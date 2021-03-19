fc050ed8eacc1c58a1af81b18bb4a7211390a254
Archie Smyth <archiesmyth26@googlemail.com>
2021-03-19 17:20:53 +0000

    added props interfaces

---
+++ interfaces/cards/index.ts
@@ -0,0 +1,5 @@
+export interface CardProps {
+      img: string
+      header: string
+      body: string
+}
---
+++ interfaces/modalcard/index.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export interface ModalCardProps {
+      action1: React.ReactNode,
+      action2:  React.ReactNode,
+      body: string
+}
