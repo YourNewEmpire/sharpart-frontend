80f2917272e2074ec4bdd0d44821f3422c63767a
Archie Smyth <archiesmyth26@googlemail.com>
2021-04-22 13:17:25 +0100

    setup hardhat for contracts to move in to this rep

---
+++ eth-orb-contracts/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+const ethers = require("ethers")
+const fs = require("fs")
+const MNEMONIC = fs.readFileSync(".secret").toString().trim();
+const API_KEY = fs.readFileSync(".apikey").toString().trim()
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    matic: {
+      url: `https://rpc-matic.maticvigil.com/v1/${API_KEY}`,
+      accounts: {
+        provider: function () {
+          return new HDWalletProvider(MNEMONIC, `https://rpc-mumbai.maticvigil.com/v1/${API_KEY}`);
+        },
+      }
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${API_KEY}`,
+      accounts: {
+        provider: function () {
+          return new HDWalletProvider(MNEMONIC, `https://rpc-mumbai.maticvigil.com/v1/${API_KEY}`);
+        },
+      }
+    }
+  }
+};
+
