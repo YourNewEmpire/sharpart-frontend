13abbb5d85a836e200fb187f6e39a1ea1fb335f6
Archie Smyth <archiesmyth26@googlemail.com>
2021-04-22 15:06:47 +0100

    added next-auth api/auth route

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+
+    }),
+
+  ],
+ 
+  callbacks: {
+     session(session, user) {
+      return session
+    },
+  },
+})
