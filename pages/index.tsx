d7daa4e467d1e142f3159a2ba51ba067e687338d
Archie Smyth <archiesmyth26@googlemail.com>
2021-06-14 17:07:22 +0100

    homepage changes. added gas data on ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,53 @@
 import Heading from '../components/Heading';
+import { GetServerSideProps } from 'next'
+import { GasData } from '../interfaces/homepage/gasData';
+import SimpleCard from '../components/Cards/SimpleCard';
+import { HomeProps } from '../interfaces/homepage';
 
-export default function Home() {
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch('https://gasstation-mainnet.matic.network')
+  const data = await res.json()
+  console.log('gas data log:', data)
+
+  if (!data) {
+    return null
+  }
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+
+export default function Home({ data }: HomeProps) {
+
 
   return (
     <>
+      {data &&
+        <div className="grid grid-cols-4  gap-4 md:gap-8 lg:gap-12">
+          <SimpleCard title="Safe Low gas price" body={data.safeLow.toString()} />
+          <SimpleCard title="Standard gas price" body={data.standard.toString()} />
+          <SimpleCard title="Fast gas price" body={data.fast.toString()} />
+          <SimpleCard title="Fastest gas price" body={data.fastest.toString()} />
+        </div>
+      }
+      {!data &&
+        <div className="m-4 md:m-10 lg:m-16">
+          <h1 className="text-center text-th-primary-light">No gas data was received</h1>
+        </div>
+      }
       <div id="div1" className="flex flex-col items-center justify-center ">
         <Heading title="This is SharpArt" >
-          <p className="text-base sm:text-xl lg:text-2xl text-th-primary-light text-shadow-md subpixel-antialiased ">
+          <p className="text-center text-base sm:text-xl lg:text-2xl text-th-primary-light text-shadow-md subpixel-antialiased ">
             The bridge between talented artists, and the secret of digital asset demand.
-</p>
+          </p>
         </Heading>
-
-
       </div>
-
-      <Heading title="This is SharpArt" />
-
-      <Heading title="This is SharpArt" />
 
     </>
   );
-
 }
