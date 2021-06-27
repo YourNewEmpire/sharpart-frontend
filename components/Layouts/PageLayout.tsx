8b0b445db481ceddc3c63a6dcde5a1c73311dd01
Archie Smyth <archiesmyth26@googlemail.com>
2021-06-27 16:04:24 +0100

    PageLayout comp - added padding

--- components/Layouts/PageLayout.tsx
+++ components/Layouts/PageLayout.tsx
@@ -8,6 +8,7 @@ export default function PageLayout({ children }: Childs): JSX.Element {
             <div className=" 
             grid grid-flow-row gap-4 md:gap-8 lg:gap-16
             justify-center items-center
+            p-2 md:p-4 lg:p-8
             ">
                 {children}
             </div>
