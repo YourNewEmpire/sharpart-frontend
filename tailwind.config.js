074f7d976a0773729a1d4ba3b509987166202fb5
Archie Smyth <archiesmyth26@googlemail.com>
2021-06-28 13:12:35 +0100

    Fixed most vulnerabilities
    Updated some packages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-
+  mode: 'jit', 
   theme: {
     screens: {
       sm: '480px',
