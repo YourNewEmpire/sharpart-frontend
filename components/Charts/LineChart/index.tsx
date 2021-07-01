f4a8a3e2a2ee01d8db2d0ee8c6a5ac9a941fd7f0
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-01 13:07:01 +0100

    Chart comp
    more props to added to charts later

---
+++ components/Charts/LineChart/index.tsx
@@ -0,0 +1,31 @@
+import { Line } from "react-chartjs-2";
+
+
+//? just data and labels for now.
+//? More options/props can be added down the pipeline
+
+export default function LineChart({ data, labels }) {
+
+      const chartConfig = {
+            labels: labels,
+            datasets: [
+                  {
+                        label: 'usd Price',
+                        data: data,
+                        fill: false,
+                        backgroundColor: '#93C5FD',
+                        borderColor: '#3B82F6',
+                        pointBorderColor: '#93C5FD',
+                        yAxisID: 'y-axis-1',
+                  },
+
+            ],
+      };
+
+      return (
+            <div>
+                  <Line type='line' data={chartConfig} />
+            </div>
+      );
+
+}
--- pages/ethorbtest.tsx
+++ pages/ethorbtest.tsx
@@ -5,6 +5,7 @@ import { selectPrice, setPrice, setPriceThunk } from '../lib/slices/ethpriceSlic
 import { EthOrbProps } from '../interfaces/pages'
 import PageLayout from "../components/Layouts/PageLayout";
 import Heading from '../components/Typography/Heading'
+import LineChart from '../components/Charts/LineChart'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
       const res = await fetch('https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=6&interval=daily')
@@ -23,69 +24,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 export default function EthOrb({ ethHistoric }: EthOrbProps) {
       const dispatch = useDispatch()
       const eth = useSelector(selectPrice)
-      const ethPrices = {
-            labels: ['30 seconds ago', '25 seconds ago', '20 seconds ago', '15 seconds ago', '10 seconds ago', '5 seconds ago', 'Now'],
-            datasets: [
-                  {
-                        label: 'usd Price',
-                        data: eth,
-                        fill: false,
-                        backgroundColor: '#93C5FD',
-                        borderColor: '#3B82F6',
-                        pointBorderColor: '#93C5FD',
-                        yAxisID: 'y-axis-1',
-                  },
+      const priceLabels = ['30 seconds ago', '25 seconds ago', '20 seconds ago', '15 seconds ago', '10 seconds ago', '5 seconds ago', 'Now'];
+      const historicLabels = ['5 days ago', '4 days ago', '3 days ago', '2 days ago', 'yesterday', 'today'];
 
-            ],
-      };
-
-
-      const historicPrices = {
-            labels: ['5 days ago', '4 days ago', '3 days ago', '2 days ago', 'yesterday', 'today'],
-            datasets: [
-                  {
-                        label: 'Prices USD',
-                        data: ethHistoric?.prices,
-                        fill: false,
-                        backgroundColor: '#93C5FD',
-                        borderColor: '#3B82F6',
-                        pointBorderColor: '#93C5FD',
-                        yAxisID: 'y-axis-1',
-                  },
-
-            ],
-      };
-      const historicVolumes = {
-            labels: ['5 days ago', '4 days ago', '3 days ago', '2 days ago', 'yesterday', 'today'],
-            datasets: [
-                  {
-                        label: 'Volumes USD',
-                        data: ethHistoric?.total_volumes,
-                        fill: false,
-                        backgroundColor: '#93C5FD',
-                        borderColor: '#3B82F6',
-                        pointBorderColor: '#93C5FD',
-                        yAxisID: 'y-axis-1',
-                  },
-
-            ],
-      };
-      const historicMarketCaps = {
-            labels: ['5 days ago', '4 days ago', '3 days ago', '2 days ago', 'yesterday', 'today'],
-            datasets: [
-                  {
-                        label: 'Market Caps USD',
-                        data: ethHistoric?.market_caps,
-                        fill: false,
-                        backgroundColor: '#93C5FD',
-                        borderColor: '#3B82F6',
-                        pointBorderColor: '#93C5FD',
-                        yAxisID: 'y-axis-1',
-
-                  },
-
-            ],
-      };
 
       const fetchEth = () => {
             dispatch(setPriceThunk())
@@ -95,6 +36,8 @@ export default function EthOrb({ ethHistoric }: EthOrbProps) {
       return (
             <PageLayout>
                   <Heading title='Test page.' hScreen={false} />
+                  <LineChart data={ethHistoric.prices} labels={historicLabels} /> 
+
 
                   <div className="grid grid-cols-1 lg:grid-cols-2 grid-flow-row gap-8 md:gap-16 lg:gap-32">
                         <div>
