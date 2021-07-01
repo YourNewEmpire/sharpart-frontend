70c313730d3912682528ee7cee4ed75445336e3c
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-01 23:12:20 +0100

    PageLayout component styling

--- components/Layouts/PageLayout.tsx
+++ components/Layouts/PageLayout.tsx
@@ -6,7 +6,7 @@ import { Childs } from '../../interfaces/childs'
 export default function PageLayout({ children }: Childs): JSX.Element {
     return (
             <div className=" 
-            grid grid-flow-row gap-4 md:gap-8 lg:gap-16
+            flex flex-col space-y-20
             justify-center items-center
             p-2 md:p-4 lg:p-8
             ">
