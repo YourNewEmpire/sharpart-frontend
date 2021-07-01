f17edf0b151bc392afe15d33db96c15134040243
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-01 15:56:41 +0100

    remove margin
    Removal of margin on headings that are without children

--- components/Typography/Heading.tsx
+++ components/Typography/Heading.tsx
@@ -6,14 +6,18 @@ interface Props extends Childs {
   hScreen: boolean
 }
 
+//? Two outcomes here.
+//? If there are NO children, just render a normal h1 with no margin and no subtitle('child')
+//? If there are children then yes, margin-bottom on the heading and children can be a subtitle or some react node
+
 export default function Heading({ title, hScreen, children }: Props): JSX.Element {
-
-
 
   if (!children)
     return (
-      <div className={`flex flex-col items-center justify-center ${hScreen ? 'h-screen' : ''}  `}>
-        <h1 className="text-center text-xl sm:text-2xl lg:text-6xl text-th-primary-medium text-shadow-md subpixel-antialiased mb-8 md:mb-14 lg:mb-20">
+      <div className={`flex flex-col items-center justify-center ${hScreen ? 'h-screen' : 'h-auto'}  `}>
+        <h1 className="text-center text-xl sm:text-2xl lg:text-6xl 
+        text-th-primary-medium text-shadow-md subpixel-antialiased 
+        ">
           {title}
         </h1>
       </div>
@@ -22,7 +26,9 @@ export default function Heading({ title, hScreen, children }: Props): JSX.Elemen
   else return (
     <>
       <div className={`flex flex-col items-center justify-center ${hScreen ? 'h-screen' : 'h-auto'}  `}>
-        <h1 className="text-center text-xl sm:text-2xl lg:text-6xl text-th-primary-medium text-shadow-md subpixel-antialiased mb-8 md:mb-14 lg:mb-20">
+        <h1 className="text-center text-xl sm:text-2xl lg:text-6xl 
+        text-th-primary-medium text-shadow-md subpixel-antialiased mb-8 md:mb-14 lg:mb-20
+        ">
           {title}
         </h1>
         {children}
