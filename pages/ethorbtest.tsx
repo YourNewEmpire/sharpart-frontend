07a46210d3bdc40eabad98c3b7de4d41272eb282
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-02 20:10:18 +0100

    ethorb ui testing

--- pages/ethorbtest.tsx
+++ pages/ethorbtest.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next'
 import { useDispatch, useSelector } from 'react-redux'
+import Moralis from 'moralis'
+import { useMoralis, } from 'react-moralis'
 import { useInterval } from '../hooks/useInterval'
 import { selectPrice, setPrice, setPriceThunk } from '../lib/slices/ethpriceSlice'
 import { priceLabels, historicLabels } from '../lib/charts/labels'
@@ -17,7 +19,11 @@ import { EthOrbProps } from '../interfaces/pages'
 import PageLayout from "../components/Layouts/PageLayout";
 import Heading from '../components/Typography/Heading'
 import LineChart from '../components/Charts/LineChart'
-import ModalCard from '../components/Cards/ModalCard';
+import SimpleCard from '../components/Cards/SimpleCard'
+
+
+
+
 
 //? Typical Server-Side-Render. P
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -41,6 +47,11 @@ export default function EthOrb({ ethHistoric }: EthOrbProps) {
       const gameWin = useSelector(selectGameWin)
       const gameResult = useSelector(selectGameResult)
       const gameLoading = useSelector(selectLoading)
+      const { isAuthenticated, user } = useMoralis()
+      const address: string = user?.get('ethAddress')
+      const authData = user?.get('authData')
+      const userSign = authData?.moralisEth.signature
+
 
       const fetchEth = () => {
             dispatch(setPriceThunk())
@@ -50,11 +61,22 @@ export default function EthOrb({ ethHistoric }: EthOrbProps) {
       return (
             <PageLayout>
                   <Heading title='Test page.' hScreen={false} />
-                  <div className='w-full h-full border-2 border-th-accent-light'>
-                        <LineChart data={eth} labels={priceLabels} />
-                        <div className='grid grid-flow-col grid-cols-2 py-24 '>
 
-                              <div className='flex border-2 border-red-400 items-center justify-center'>
+                  <div className='w-full h-full border-2 border-th-primary-light'>
+                        <div className='grid grid-flow-col grid-cols-2 '>
+                             <div>
+
+                             </div>
+                             <div>
+
+                             </div>
+                        </div>
+                        <div className=' flex mx-auto max-w-xs'>
+                        <LineChart data={eth} labels={priceLabels} />
+
+                        </div>
+                        <div className='grid grid-flow-col grid-cols-2 '>
+                              <div className='flex border-2 items-center justify-center'>
                                     <button
                                           onClick={() => dispatch(setChoiceUp())}
                                           className={choice === true ? 'w-full p-2 text-center  text-xs md:text-sm lg:text-xl   text-th-primary-light rounded-lg bg-opacity-100 bg-th-accent-success  focus:outline-none   transition duration-300 ease-in-out'
@@ -77,7 +99,6 @@ export default function EthOrb({ ethHistoric }: EthOrbProps) {
                               </div>
                         </div>
                   </div>
-                  
             </PageLayout>
       );
 
