d1a4788462c211dcd92e1ceb169100e4493f749a
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-02 22:24:37 +0100

    Heading fontSize optional prop

--- components/Typography/Heading.tsx
+++ components/Typography/Heading.tsx
@@ -4,20 +4,21 @@ import { Childs } from '../../interfaces/childs'
 interface Props extends Childs {
   title: string
   hScreen: boolean
+  fontSize?: string
 }
 
 //? Two outcomes here.
 //? If there are NO children, just render a normal h1 with no margin and no subtitle('child')
 //? If there are children then yes, margin-bottom on the heading and children can be a subtitle or some react node
 
-export default function Heading({ title, hScreen, children }: Props): JSX.Element {
+export default function Heading({ title, hScreen, children, fontSize }: Props): JSX.Element {
 
   if (!children)
     return (
       <div className={`flex flex-col items-center justify-center ${hScreen ? 'h-screen' : 'h-auto'}  `}>
-        <h1 className="text-center text-xl sm:text-2xl lg:text-6xl 
+        <h1 className={`text-center ${fontSize? fontSize : 'text-xl sm:text-2xl lg:text-6xl '}
         text-th-primary-medium text-shadow-md subpixel-antialiased 
-        ">
+        `}>
           {title}
         </h1>
       </div>
