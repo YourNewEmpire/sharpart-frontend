d29a83af686f7f732083da7456e3e5aac3606b68
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-07 12:43:08 +0100

    GameButtons text-size

--- components/Game/Buttons/GameButtons.tsx
+++ components/Game/Buttons/GameButtons.tsx
@@ -25,8 +25,8 @@ export default function GameButtons({ clickHandler }): JSX.Element {
                     <button
                         onClick={() => dispatch(setChoiceUp())}
                         className={choice === true ?
-                            'w-full p-2 sm:p-6 md:p-14 lg:p-24 text-center text-xs md:text-sm lg:text-xl text-th-primary-light rounded-lg bg-opacity-100 bg-th-accent-success  focus:outline-none   transition duration-300 ease-in-out'
-                            : 'w-full p-2 sm:p-6 md:p-14 lg:p-24 text-center ring-2 lg:ring-4 ring-th-accent-success  text-xs md:text-sm lg:text-xl   text-th-primary-light  rounded-lg bg-opacity-0   focus:outline-none   transition duration-300 ease-in-out'
+                            'w-full p-2 sm:p-6 md:p-14 lg:p-24 text-center text-xs md:text-sm lg:text-4xl text-th-primary-light rounded-lg bg-opacity-100 bg-th-accent-success focus:outline-none transition duration-300 ease-in-out'
+                            : 'w-full p-2 sm:p-6 md:p-14 lg:p-24 text-center ring-2 lg:ring-4 ring-th-accent-success text-xs md:text-sm lg:text-4xl text-th-primary-light rounded-lg bg-opacity-0 focus:outline-none transition duration-300 ease-in-out'
                         }
                     >
                         Mooning
@@ -36,8 +36,8 @@ export default function GameButtons({ clickHandler }): JSX.Element {
                     <button
                         onClick={() => dispatch(setChoiceDown())}
                         className={choice === false ?
-                            'w-full p-4  sm:p-6 md:p-14 lg:p-24  text-center text-xs md:text-sm lg:text-xl text-th-primary-light rounded-lg bg-opacity-100 bg-th-accent-failure focus:outline-none transition duration-300 ease-in-out'
-                            : 'w-full p-4 sm:p-6 md:p-14 lg:p-24 ring-2 lg:ring-4 ring-th-accent-failure text-center text-xs md:text-sm lg:text-xl text-th-primary-light rounded-lg bg-opacity-0 focus:outline-none transition duration-300 ease-in-out'
+                            'w-full p-4  sm:p-6 md:p-14 lg:p-24  text-center text-xs md:text-sm lg:text-4xl text-th-primary-light rounded-lg bg-opacity-100 bg-th-accent-failure focus:outline-none transition duration-300 ease-in-out'
+                            : 'w-full p-4 sm:p-6 md:p-14 lg:p-24 ring-2 lg:ring-4 ring-th-accent-failure text-center text-xs md:text-sm lg:text-4xl text-th-primary-light rounded-lg bg-opacity-0 focus:outline-none transition duration-300 ease-in-out'
                         } 
                     >
                         Dropping
