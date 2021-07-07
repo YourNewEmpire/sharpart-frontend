c0f95189477a282851035b1a3b662a83a52ae057
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-07 13:16:24 +0100

    Submit.tsx code formatting

--- components/Game/Buttons/Submit.tsx
+++ components/Game/Buttons/Submit.tsx
@@ -9,47 +9,49 @@ import {
 //TODO This totally needs optimising for more reusability.
 //? Just being introduced.
 
-export default function Submit({clickHandler}): JSX.Element {
+export default function Submit({ clickHandler }): JSX.Element {
       const choice = useSelector(selectChoice)
-      
+
       return (
-          <div>
-                {choice === null &&
+            <>
+                  {choice === null &&
                         <div className='flex items-center justify-center'>
 
                               <button
                                     className={`
-      p-4 lg:px-14 lg:py-6 text-center text-4xl 
-      text-th-primary-dark 
-      bg-gradient-to-r from-th-primary-dark via-th-primary-medium to-th-primary-light
-      antialiased focus:outline-none
-      opacity-20
-      `}
-                                  
+                              p-4 lg:px-24 lg:py-12 text-center text-4xl 
+                              text-th-primary-dark 
+                              bg-gradient-to-r from-th-primary-dark via-th-primary-medium to-th-primary-light
+                              antialiased focus:outline-none
+                              opacity-20
+                              `}
+
                               >
                                     Submit Order
                               </button>
 
                         </div>
                   }
-                  {choice !== null && <div className='flex items-center justify-center'>
+                  {choice !== null &&
+                        <div className='flex items-center justify-center'>
 
-                        <button
-                              className={`
-                              p-4 lg:px-14 lg:py-6 text-center text-4xl 
+                              <button
+                                    className={`
+                              p-4 lg:px-24 lg:py-12 text-center text-4xl 
                               text-th-primary-dark 
                               bg-gradient-to-r from-th-primary-dark via-th-primary-medium to-th-primary-light
                               hover:ring-8 ring-offset-th-primary-medium
-                             transform  hover:scale-110
+                              transform  hover:scale-110
                               transition duration-300 ease-in-out
                               antialiased focus:outline-none
                               `}
-                              onClick={clickHandler}
-                        >
-                              Submit Order
-                        </button>
+                                    onClick={clickHandler}
+                              >
+                                    Submit Order
+                              </button>
 
-                  </div>}
-          </div>
+                        </div>
+                  }
+            </>
       )
 }
