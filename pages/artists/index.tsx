a7760ae49a9f9a163664cc7f6ac88187a5509fbe
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-08 12:01:07 +0100

    border-radius extend

--- pages/artists/index.tsx
+++ pages/artists/index.tsx
@@ -1,22 +1,35 @@
 import Mp4Card from '../../components/Cards/Mp4Card';
 import ListCard from '../../components/Cards/ListCard';
 import Heading from '../../components/Typography/Heading';
-import ReactPlayer from 'react-player'
 import PageLayout from "../../components/Layouts/PageLayout";
 import ImageLinkCard from '../../components/Cards/ImageLinkCard'
-import {cardItems} from '../../lib/artists/artistLib'
-export default function Artists() {
+import { cardItems } from '../../lib/artists/artistLib'
 
-  
+
+export default function Artists() {
 
 
   return (
     <PageLayout>
+
+      <div>
+
+        <Heading title="Current Artists" hScreen={false} />
+        <div>
+          here are the avatars
+        </div>
+        <button className=' lg:h-32 lg:w-32 rounded-full focus:outline-none'>
+        <img className='w-8 h-8 lg:h-32 lg:w-32  object-cover rounded-full ' src="https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg" alt="" />
+
+        </button>
+      </div>
+
+
       <Heading title="Current Artists" hScreen={false} />
       <ListCard title="Artists" body={cardItems} />
       <div>
         {cardItems.map((item, index) =>
-          <div key={index} id={item.anchorLink} className="flex  items-center justify-center h-screen">
+          <div key={index} id={item.anchorLink} className="flex items-center justify-center h-screen">
             {item.isImage === false &&
               <Mp4Card
                 img={item.img}
@@ -36,8 +49,6 @@ export default function Artists() {
           </div>
         )}
       </div>
-
-      <div className="h-screen border-2 " ></div>
 
     </PageLayout>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,16 @@ module.exports = {
       roman: 'upper-roman'
     },
     extend: {
-
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        DEFAULT: '4px',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'large': '12px',
+      },
       ringWidth: {
         'DEFAULT': '2px',
         '6': '6px',
