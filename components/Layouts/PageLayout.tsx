4dccece25ee3ba1abbd384255b52d63bf3cf6da3
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-12 20:24:49 +0100

    PageLayout styles prop added

--- components/Layouts/PageLayout.tsx
+++ components/Layouts/PageLayout.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Childs } from '../../interfaces/childs'
 
+import { PageLayoutProps } from '../../interfaces/layouts'
 
+export default function PageLayout({ children, styles }: PageLayoutProps): JSX.Element {
 
-export default function PageLayout({ children }: Childs): JSX.Element {
-    return (
-            <div className=" 
+    if (styles) return (
+        <div className={styles}>
+            {children}
+        </div>
+    )
+    else return (
+        <div className=" 
             flex flex-col space-y-20
             justify-center items-center
             p-2 md:p-4 lg:p-8
             ">
-                {children}
-            </div>
+            {children}
+        </div>
     )
 }
