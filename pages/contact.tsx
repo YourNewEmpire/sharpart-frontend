67bf18cc7f5c2c5c5fd7cb6913e26639803c9d49
Archie Smyth <archiesmyth26@googlemail.com>
2021-07-14 13:09:53 +0100

    commenting and removal of unused

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
 import {FaDiscord} from 'react-icons/fa' ;
 
+// todo - Add discord and email links.
 export default function Contact() {
   return (
     <>
       <div id="div1" className="flex items-center justify-center py-64 px-24">
         <h1 className="text-6xl text-darkblue-100">Get in contact</h1>
-    <FaDiscord/>
+        <FaDiscord/>
       </div>
     </>
   );
