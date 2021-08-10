f6fa1dc60a837d5f737191e8dd3ad235c30c8a2b
Archie Smyth <archiesmyth26@googlemail.com>
2021-08-10 08:16:16 +0100

    next.config.js serverconfig project root

--- next.config.js
+++ next.config.js
@@ -7,11 +7,13 @@ module.exports = {
     APPLICATION_SECRET: process.env.APPLICATION_SECRET,
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-    SECRET : process.env.SECRET,
+    SECRET: process.env.SECRET,
     NEXT_PUBLIC_MORALIS_APP_ID: process.env.NEXT_PUBLIC_MORALIS_APP_ID,
     NEXT_PUBLIC_MORALIS_SERVER_URL: process.env.NEXT_PUBLIC_MORALIS_SERVER_URL,
     MATIC_API_KEY: process.env.MATIC_API_KEY,
     MNEMONIC: process.env.MNEMONIC
   },
-
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+  }
 }
