f3d286a0f5f74bb1718480fe345c78941e924909
Archie Smyth <archiesmyth26@googlemail.com>
2021-08-11 15:17:04 +0100

    test env in next config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com', 'ipfs.io/ipfs/'],
   },
-  env: {
-    APPLICATION_SECRET: process.env.APPLICATION_SECRET,
-    GOOGLE_ID: process.env.GOOGLE_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-    SECRET: process.env.SECRET,
-    NEXT_PUBLIC_MORALIS_APP_ID: process.env.NEXT_PUBLIC_MORALIS_APP_ID,
-    NEXT_PUBLIC_MORALIS_SERVER_URL: process.env.NEXT_PUBLIC_MORALIS_SERVER_URL,
-    MATIC_API_KEY: process.env.MATIC_API_KEY,
-    MNEMONIC: process.env.MNEMONIC
-  },
+
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname
   }
