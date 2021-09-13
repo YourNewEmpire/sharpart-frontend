ac9938586a7eea3fe71ca8fd21c7b5b537b4bff5
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-13 21:09:56 +0100

    toast colour for copyToClipboard

--- lib/helpers/copyToClipboard/index.ts
+++ lib/helpers/copyToClipboard/index.ts
@@ -3,19 +3,7 @@ import { toast, Zoom} from 'react-toastify';
 const  copyToBoard = (text: string) => {
       navigator.clipboard.writeText(text).then(
             function () {
-                  toast.dark( 'copied: ' + text , {
-                        position: "top-right",
-                        autoClose: 3000,
-                        transition: Zoom,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: true,
-                        progress: undefined,
-                  })
-            },
-            function () {
-                  toast.dark( 'copied: ' + text , {
+                  toast.info( 'copied: ' + text , {
                         position: "top-right",
                         autoClose: 3000,
                         transition: Zoom,
