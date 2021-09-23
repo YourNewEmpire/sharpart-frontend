3e0c299313cea4439a45f2bba73448b6f1ea9fc0
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-23 13:10:44 +0100

    copyToBoard helper changed
    validate payload

--- lib/helpers/copyToClipboard/index.ts
+++ lib/helpers/copyToClipboard/index.ts
@@ -1,6 +1,19 @@
-import { toast, Zoom} from 'react-toastify';
+import { toast, Zoom } from 'react-toastify';
 
 const  copyToBoard = (text: string) => {
+      if(text.length < 1) {
+            toast.error('No text to copy', {
+                  position: "top-right",
+                  autoClose: 3000,
+                  transition: Zoom,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: false,
+                  draggable: true,
+                  progress: undefined, 
+            })
+            return false
+      }
       navigator.clipboard.writeText(text).then(
             function () {
                   toast.info( 'copied: ' + text , {
