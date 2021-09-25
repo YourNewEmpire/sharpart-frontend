77657f84409a13620c16b71afdf78415e1b345df
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-25 20:34:47 +0100

    alert card change. added optional 'icon' element.

--- components/Cards/AlertCard.tsx
+++ components/Cards/AlertCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AlertCardProps } from '../../interfaces/cards'
-
-export default function AlertCard({ title, body, success, failure, info, warning }: AlertCardProps): JSX.Element {
+import { HomeIcon } from '@heroicons/react/solid'
+export default function AlertCard({ title, body, icon, success, failure, info, warning }: AlertCardProps): JSX.Element {
 
 
       if (title && body && warning) return (
@@ -9,9 +9,15 @@ export default function AlertCard({ title, body, success, failure, info, warning
                   className="
             bg-th-accent-warning-light text-th-accent-warning-dark
             border-2 md:border-8 border-th-accent-warning-medium px-4 py-4 rounded relative"
-             role="alert"
+                  role="alert"
             >
-                  <h1 className="font-bold text-sm md:text-3xl mb-4">{title}</h1>
+                  <div className='flex flex-row space-x-4'>
+                        {icon}
+                        <h1 className="font-bold text-sm md:text-3xl mb-4">
+
+                              {title}
+                        </h1>
+                  </div>
                   <span className="block sm:inline">{body}</span>
             </div>
       )
@@ -22,7 +28,13 @@ export default function AlertCard({ title, body, success, failure, info, warning
             border-2 md:border-8 px-4 py-3 rounded relative
             " role="alert"
             >
-                  <h1 className="font-bold text-sm md:text-3xl mb-4">{title}</h1>
+                  <div className='flex flex-row space-x-4'>
+                        {icon}
+                        <h1 className="font-bold text-sm md:text-3xl mb-4">
+
+                              {title}
+                        </h1>
+                  </div>
                   <span className="block sm:inline">{body}</span>
             </div>
       )
@@ -31,9 +43,15 @@ export default function AlertCard({ title, body, success, failure, info, warning
                   className="
             bg-th-accent-info-light border-th-accent-info-medium text-th-accent-info-dark
             border-2 md:border-8 px-4 py-3 rounded relative"
-             role="alert"
+                  role="alert"
             >
-                  <h1 className="font-bold text-sm md:text-3xl mb-4">{title}</h1>
+                  <div className='flex flex-row space-x-4'>
+                        {icon}
+                        <h1 className="font-bold text-sm md:text-3xl mb-4">
+
+                              {title}
+                        </h1>
+                  </div>
                   <span className="block sm:inline">{body}</span>
             </div>
       )
@@ -41,10 +59,16 @@ export default function AlertCard({ title, body, success, failure, info, warning
             <div
                   className="
             bg-th-accent-success-light border-th-accent-success-medium text-th-accent-success-dark
-            border-2 md:border-8  px-4 py-3 rounded relative" 
-            role="alert"
+            border-2 md:border-8  px-4 py-3 rounded relative"
+                  role="alert"
             >
-                  <h1 className=" font-bold text-sm md:text-3xl mb-4">{title}</h1>
+                  <div className='flex flex-row space-x-4'>
+                        {icon}
+                        <h1 className="font-bold text-sm md:text-3xl mb-4">
+
+                              {title}
+                        </h1>
+                  </div>
                   <span className="block sm:inline">{body}</span>
             </div>
       )
