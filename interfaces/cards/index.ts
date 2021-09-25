9c56494151389868ea71b9521fc16d288dc7e496
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-25 20:33:26 +0100

    add optional icon prop to alert card

--- interfaces/cards/index.ts
+++ interfaces/cards/index.ts
@@ -47,6 +47,7 @@ export interface SimpleCardProps  {
 export interface AlertCardProps {
       title: string
       body: string
+      icon?: JSX.Element
       success?: boolean
       info?: boolean
       warning?: boolean
